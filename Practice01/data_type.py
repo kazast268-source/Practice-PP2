f56cb8eb13013e509204c7429a1e5757b5fe9755
Nurdaulet <kazast269@gmail.com>
2026-01-23 10:58:27 +0500

    Add data_type.py example

---
+++ Practice01/data_type.py
@@ -0,0 +1,16 @@
+"""Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType"""
+x="nurdaulet"
+y=18
+m=34,8
+i=["apple", "banana", "cherry"]
+print(type(x))
+print(type(m))
+print(type(y))
+print(type(i))
